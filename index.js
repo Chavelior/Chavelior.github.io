ceebf5cfcc3d429bb011e02c1b17f02bb70221b4
Akhila Devi Devabhaktuni <akhiladevi95@gmail.com>
2019-01-29 18:06:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function eventdirect(evt, task) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-
+  document.getElementById("mainInfo").style.display = "none"
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(task).style.display = "block";
   evt.currentTarget.className += " active";
